c185f4567ac16d31e8dd66377d02300967cc4552
brunda30 <brundashreebrunda2@gmail.com>
2025-05-16 09:52:12 +0530

    Create number two.c

---
+++ number two.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+ int a,b;
+ printf("enter a value of a and b\n");
+ scanf("%d",&a,&b);
+ if(a>20||b<10){
+     printf("codition is true");
+ }else{
+     printf("condition is false");
+ }
+}
+ 
