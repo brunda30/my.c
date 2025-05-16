ddb8efd788abbe4498cd63b5990819396976bdf5
brunda30 <brundashreebrunda2@gmail.com>
2025-05-16 15:23:11 +0530

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int *arr=(int*)malloc(3* sizeof(int));
+    arr=(int*)realloc(arr,7*sizeof(int));
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
+    arr=(int*)realloc(arr,5*sizeof(int));
+    arr[3]=40;
+    arr[4]=50;
+    free(arr);
+    for(int i=0;i<5;i++)
+    printf("%d",arr[i]);
+    return 0;
+}
