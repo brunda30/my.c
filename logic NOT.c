d1c3b95678779d36659f935c9377a6357e43beea
brunda30 <brundashreebrunda2@gmail.com>
2025-05-16 09:56:19 +0530

    Create logic NOT.c

---
+++ logic NOT.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+ int x;
+ printf("enter a value of x\n");
+ scanf("%d",&x);
+ if(!x==0){
+     printf("equal to zero");
+ }else{
+     printf("not equal to zero");
+ }
+}
+ 
