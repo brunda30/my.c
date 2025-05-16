5b4b933c829f46bd77ae9d8201a22e20be32a672
brunda30 <brundashreebrunda2@gmail.com>
2025-05-16 12:28:15 +0530

    Update and rename prime .c to reverse.c

---
+++ reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int i,num,isprime=1;
+int main(){
+    printf("enter the name starting from positive 2 ");
+    scanf("%d",&num);
+    int limit=(int)num/2;
+    for(i=2;i<=num/2;i++){
+        if(num%2==0){
+            isprime=0;
+        }
+    }
+    if(isprime){
+        printf("its a prime\n");
+        }else{
+            printf("its not a prime\n");
+        }
+}
