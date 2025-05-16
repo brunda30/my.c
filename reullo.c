07ef1f5ec4ca0c0228055e1737bcec40281e05ab
brunda30 <brundashreebrunda2@gmail.com>
2025-05-16 16:03:40 +0530

    Create reullo.c

---
+++ reullo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdio.h>
+int main(){
+    struct brunda{
+        int age;
+        float weight;
+    };
+    struct challakere{
+        int marks;
+    struct brunda r1;
+    };
+    struct challakere p1;
+    scanf("%d",&p1.marks);
+    scanf("%d",&p1.r1.age);
+    scanf("%f",&p1.r1.weight);
+    return 0;
+}
