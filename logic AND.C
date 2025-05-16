2db3926f8c443e221c4bfe338e6bbf55c1829bfb
brunda30 <brundashreebrunda2@gmail.com>
2025-05-16 10:03:08 +0530

    Create logic AND.C

---
+++ logic AND.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+ int a,b;
+ printf("enter a value of a and b\n");
+ scanf("%d",&a,&b);
+ if(a>10 &&b<20){
+     printf("codition is  true");
+ }else{
+     printf("condition is false");
+ }
+}
